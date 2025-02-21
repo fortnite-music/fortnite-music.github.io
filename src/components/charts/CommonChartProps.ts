dd874ed3b73341e91fd1ed58a049db81de7efcc4
koba <jaspycatty@gmail.com>
2025-02-21 14:36:59 -0500

    test

--- src/components/charts/CommonChartProps.ts
+++
@@ -1,3 +0,0 @@
-export interface ICommonChartProps {
-  selectedYear: number;
-}
--- src/state/hot.ts
+++
@@ -1,4 +0,0 @@
-import { atom } from 'jotai';
-import { IHotTrackEntry } from '../models/HotTracks';
-
-export const hotTracksAtom = atom<IHotTrackEntry[]>([]);
